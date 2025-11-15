92e45116fae3919511a549b26d867445611c3940
dave.visualB <nicka18chinese@gmail.com>
2025-11-15 15:28:42 +0800

    feat(remove): remove dev tools

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+//remove dev tools
 
 // https://vite.dev/config/
 export default defineConfig({
